af4eeb5902c9bff12e00d8db906f7d4592f79bd5
Alex Vernacchia <vernak2539@users.noreply.github.com>
2022-10-10 11:21:48 +0100

    remove sentry dns from public code (whoops)
    Disabled the key as well... lovely

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import "@sentry/tracing";
 import start from "./src/start.js";
 
 Sentry.init({
-  dsn: "https://3f7afb1bbecf4f83bbff2b00c65a1210@o1429321.ingest.sentry.io/6779964",
+  dsn: process.env.SENTRY_DSN,
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
